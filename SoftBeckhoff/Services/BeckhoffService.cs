9b9b951dfc437520dabc2b7642077ff178312c55
Federico Barresi <federico.barresi@evopro-ag.de>
2020-04-13 20:18:55 +0200

    added server class from package

--- SoftBeckhoff/Services/BeckhoffService.cs
+++ SoftBeckhoff/Services/BeckhoffService.cs
@@ -1,16 +1,40 @@
 using System;
+using System.Reflection;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using TwinCAT.Ads.Server;
 
 namespace SoftBeckhoff.Services
 {
     public class BeckhoffService : IPlcService
     {
-        public BeckhoffService()
+        public BeckhoffService(ILogger<BeckhoffService> logger)
         {
+	        var server = new BeckhoffServer(logger);
         }
 
         public void Dispose()
 		{
 			
 		}   
+    }
+
+    public class BeckhoffServer
+    {
+	    private readonly ILogger _logger;
+
+	    public BeckhoffServer(ILogger logger)
+	    {
+		    _logger = logger;
+		    var server = (AmsServerNet) typeof(AmsServerNet)
+			    .GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, 
+				    null, 
+				    CallingConventions.Any, 
+				    new []{typeof(ILogger)}, 
+				    null)
+			    ?.Invoke(new[] {logger});
+		    var result = server.AmsConnect(851, "SoftPlc");
+		    var connected = server.IsServerConnected;
+	    }
     }
 }
